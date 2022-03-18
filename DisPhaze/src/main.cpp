1db64df8ade28fe7959dfd274ad28828ea641f10
Dominic Webb <dwebb@reapit.com>
2022-03-18 16:23:00 +0000

    Started layout of v3 board

--- DisPhaze/src/main.cpp
+++ DisPhaze/src/main.cpp
@@ -13,8 +13,15 @@
  * 6. When the calibration is complete press the calibration button again to save
  */
 
-/* To do:
- * sample extraction python script
+/* V3 Changes:
+ * Octave Up PC10 > PA0
+ * Octave Dn PC11 > PC3
+ * DAC2_Mix PB8 > PC13
+ * Swap:
+ * PD1_Type
+ * VCA
+ * PD2_CV
+ * PD2_POT
  */
 
 Config config;
